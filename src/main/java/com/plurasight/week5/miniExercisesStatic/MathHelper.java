1a3183ca7eaba3eaeb6c52a05f5cd5db4fdcd98f
ErmiyasHailemichael <91638042+ErmiyasHailemichael@users.noreply.github.com>
2025-10-23 12:03:19 -0700

    File organized

---
+++ src/main/java/com/plurasight/week5/miniExercisesStatic/MathHelper.java
@@ -0,0 +1,10 @@
+package com.plurasight.week5.miniExercisesStatic;
+
+public class MathHelper {
+     static int add(int a, int b){
+        return a + b;
+    }
+    static int square(int number){
+         return number * number;
+    }
+}
