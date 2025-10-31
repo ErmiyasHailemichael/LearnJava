b569c5ee12c4826f1b01abc2c07431de0dd84d03
ErmiyasHailemichael <91638042+ErmiyasHailemichael@users.noreply.github.com>
2025-10-31 12:08:12 -0700

    working on workshop 5w - car dealership part 2 but from scratch

---
+++ src/main/java/com/pluralsight/week6/WS6CarDealership2/Dealership2.java
@@ -0,0 +1,45 @@
+package com.pluralsight.week6.WS6CarDealership2;
+
+import java.util.ArrayList;
+
+public class Dealership2 {
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle2> inventory;
+
+    public Dealership2(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();
+    }
+
+    public ArrayList<Vehicle2> getInventory() {
+        return inventory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
